972bdf55a3ddd41b739ddf283b4b28ffae1f5fc2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-13 20:50:53 -0500

    Complete 'db/schema.sql'.

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,18 @@
+DROP DATABASE IF EXISTS hotwheels_dev;
 
+CREATE DATABASE hotwheels_dev;
+
+\c hotwheels_dev;
+
+CREATE TABLE hotwheels (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	imgUrl TEXT,
+	year INT,
+	engine TEXT,
+	description TEXT,
+	hybrid BOOLEAN,
+	is_favorite BOOLEAN
+);
 
 
