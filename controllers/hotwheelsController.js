b2e8d1aeaf2aa3f16981c5c9a09437a112569291
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-13 20:28:20 -0500

    Complete 'allBookmarks' index route in 'hotwheelsController.js'.

--- controllers/hotwheelsController.js
+++ controllers/hotwheelsController.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
-const {
-	getAllHotwheels,
-	getOneHotwheel,
-	createHotwheel,
-	deleteHotwheel,
-	updateHotwheel,
-} = require('../queries/hotwheels.js');
+const { getAllHotwheels } = require('../queries/hotwheels.js');
+
+const hotwheels = express.Router();
+
+hotwheels.get('/', async (req, res) => {
+	const allHotwheels = await getAllHotwheels();
+	if (allHotwheels[0]) {
+		res.status(200).json({ success: true, data: { payload: allHotwheels } });
+	} else {
+		res.status(500).json({ success: false, data: { error: 'Server error' } });
+	}
+});
+
+module.exports = hotwheels;
